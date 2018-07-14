4faa937729375aa4fb51af9f07ebe9ba02e66479
xuanmo <yh_xuanmo@163.com>
2018-07-14 11:16:19 +0800

    '更改模板页统一继承自base.html'

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'about',
+    'article',
+    'resource',
+    'timeline'
 ]
 
 MIDDLEWARE = [
