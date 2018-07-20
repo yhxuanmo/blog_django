0db610975ac807be9556eeec3821f5e52cb542e2
xuanmo <yh_xuanmo@163.com>
2018-07-20 19:37:53 +0800

    '创建思维导图，重构了modle'

--- my_blog/article/models.py
+++ my_blog/article/models.py
@@ -1,21 +1,37 @@
 from datetime import datetime
 from django.db import models
 
-# Create your models here.
+
+class ArticleTag(models.Model):
+    """
+    文章分类标签
+    """
+    tag_name = models.CharField(max_length=32, null=False)  # 文章标签
+    create_time = models.DateTimeField(auto_now=datetime.now())  # 创建时间
+    is_delete = models.BooleanField(default=0)  # 是否禁用
+
+    class Meta:
+        db_table = 'tb_article_tag'
+
+
+
 class MyArticle(models.Model):
+    """
+    文章
+    """
     title = models.CharField(max_length=64, null=False)  # 标题
     author = models.CharField(max_length=32, null=False)  # 作者
     abstract = models.CharField(max_length=128, null=False)  # 摘要
-    tag = models.CharField(max_length=32, null=False)  # 标签
+
     article = models.TextField(null=False)  # 文章
     create_time = models.DateTimeField(auto_now_add=True, null=False)  # 创建时间
     update_time = models.DateTimeField(auto_now=datetime.now(), null=False)  # 更新时间
     count_of_read = models.IntegerField(default=0, null=True)  # 阅读数
     count_of_comment = models.IntegerField(default=0, null=True)  # 评论数
+    tag = models.ForeignKey(ArticleTag)  # 标签
 
     class Meta:
         db_table = 'tb_article'
-
 
     def main_to_dict(self):
         return {
@@ -45,6 +61,9 @@ class MyArticle(models.Model):
 
 
 class ReaderComment(models.Model):
+    """
+    评论
+    """
     reader_name = models.CharField(max_length=32, null=False)  # 评论用户名
     comment_time = models.DateTimeField(auto_now=datetime.now(), null=False)  # 评论时间
     comment = models.TextField(null=False)  # 评论
@@ -53,3 +72,5 @@ class ReaderComment(models.Model):
 
     class Meta:
         db_table = 'tb_reader_comment'
+
+
